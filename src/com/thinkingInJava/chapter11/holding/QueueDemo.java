13fcef82cba3e45d7041e2f90e85a726791db92e
zhangrongzhao <zrz009@126.com>
2018-05-09 23:10:16 +0800

    QueueDemo

---
+++ src/com/thinkingInJava/chapter11/holding/QueueDemo.java
@@ -0,0 +1,31 @@
+package com.thinkingInJava.chapter11.holding;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+/**
+ * Created by rongzhao on 2018/5/9.
+ */
+public class QueueDemo {
+    public static void printQ(Queue queue){
+        while(queue.peek()!=null){
+            System.out.println(queue.remove()+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Queue<Integer> queue=new LinkedList<Integer>();
+        Random rand=new Random(47);
+        for(int i=0;i<10;i++){
+            queue.offer(rand.nextInt(i+10));
+        }
+        System.out.println(queue);
+        Queue<Character> qc=new LinkedList<Character>();
+        for(char c:"Brontosaurus".toCharArray()){
+            qc.offer(c);
+        }
+        System.out.println(qc);
+    }
+}
