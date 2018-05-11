ce92b40f95313807443ea059911ece1da6475d65
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-05-11 10:42:00 +0800

    ArrayAsList
    Arrays.asList()底层使用物理数组实现。

---
+++ src/com/thinkingInJava/chapter11/holding/ModifyingArrayAsList.java
@@ -0,0 +1,20 @@
+package com.thinkingInJava.chapter11.holding;
+
+import java.util.*;
+
+public class ModifyingArrayAsList {
+    public static void main(String[] args){
+        Random rand=new Random(47);
+        Integer[] ia={0,1,2,3,4,5,6,7,8,9};
+        List<Integer> list1=new ArrayList<Integer>(Arrays.asList(ia));
+        System.out.println("Before shuffling: "+list1);
+        Collections.shuffle(list1,rand);
+        System.out.println("After shuffling:"+list1);
+        System.out.println("array: "+Arrays.toString(ia));
+
+        List<Integer> list2 = Arrays.asList(ia);
+        System.out.println("Before shuffling: "+list2);
+        Collections.shuffle(list2,rand);
+        System.out.println("After shuffling: "+list2);
+    }
+}
