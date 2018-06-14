07a45416522c388a389714b0c1bd8df177fdcb8b
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-06-14 19:28:28 +0800

    捕获转换

---
+++ src/com/thinkingInJava/chapter15/generics/CaptureConversion.java
@@ -0,0 +1,25 @@
+package com.thinkingInJava.chapter15.generics;
+
+public class CaptureConversion {
+    static <T> void f1(Holder<T> holder){
+        T t=holder.get();
+        System.out.println(t.getClass().getSimpleName());
+    }
+
+    static void f2(Holder<?> holder){
+        f1(holder);
+    }
+
+    public static void main(String[] args){
+        Holder raw=new Holder<Integer>(1);
+        f1(raw);
+        f2(raw);
+
+        Holder rawBasic=new Holder();
+        rawBasic.set(new Object());
+        f2(rawBasic);
+
+        Holder<?> wildcarded=new Holder<Double>(1.0);
+        f2(wildcarded);
+    }
+}
