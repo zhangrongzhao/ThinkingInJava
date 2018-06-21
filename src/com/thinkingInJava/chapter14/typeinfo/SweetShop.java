0965ed9892c91dd8dccfb458f0437fc99cf77cd3
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-06-21 11:14:43 +0800

    14.2Class对象

---
+++ src/com/thinkingInJava/chapter14/typeinfo/SweetShop.java
@@ -0,0 +1,25 @@
+package com.thinkingInJava.chapter14.typeinfo;
+
+class Candy{
+    static { System.out.println("Loading Candy"); }
+}
+
+class Gum{
+    static { System.out.println("Loading Gum"); }
+}
+
+class Cookie{
+    static { System.out.println("Loading Cookie"); }
+}
+
+public class SweetShop {
+    public static void main(String[] args){
+        new Candy();
+        try{
+            Class.forName("com.thinkingInJava.chapter14.typeinfo.Gum");
+        }catch(ClassNotFoundException e){
+            System.out.println("Couldn't find Gum");
+        }
+        new Cookie();
+    }
+}
