c8baf568b8468d82e79bb2f22948ef8853f32907
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-07-17 10:32:15 +0800

    17.7.1优先级队列 17.7.2双向队列

---
+++ src/com/thinkingInJava/util/Deque.java
@@ -0,0 +1,16 @@
+package com.thinkingInJava.util;
+
+import java.util.LinkedList;
+
+public class Deque<T> {
+    private LinkedList<T> deque=new LinkedList<T>();
+    public void addFirst(T e){deque.addFirst(e);}
+    public void addLast(T e){deque.addLast(e);}
+    public T getFirst(){return deque.getFirst();}
+    public T getLast(){return deque.getLast();}
+    public T removeFirst(){return deque.removeFirst();}
+    public T removeLast(){return deque.removeLast();}
+    public int size(){return deque.size();}
+    public String toString(){return deque.toString();}
+    //And other methods as necessary
+}
