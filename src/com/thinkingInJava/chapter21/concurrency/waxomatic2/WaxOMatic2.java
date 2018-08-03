c63202fc6a1d709ae51bed5befb968ca63df4bb7
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-08-03 10:20:37 +0800

    21.5.3生产者和消费者：使用显示的Lock和Condition对象

--- src/com/thinkingInJava/chapter21/concurrency/waxomatic2/WaxOMatic2.java
+++ src/com/thinkingInJava/chapter21/concurrency/waxomatic2/WaxOMatic2.java
@@ -1,5 +1,8 @@
 package com.thinkingInJava.chapter21.concurrency.waxomatic2;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -50,5 +53,52 @@ class Car{
     }
 }
 
+class WaxOn implements Runnable{
+    private Car car;
+    public WaxOn(Car c){
+        this.car=c;
+    }
+    public void run(){
+        try{
+            while(!Thread.interrupted()){
+                System.out.println("Wax on!");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.waxed();
+                car.waitForBuffing();
+            }
+        }catch (InterruptedException e){
+            System.out.println("Exiting via interrupt");
+        }
+        System.out.println("Ending Wax On task");
+    }
+}
+
+class WaxOff implements Runnable{
+    private Car car;
+    public WaxOff(Car c){this.car=c;}
+    public void run(){
+       try{
+           while(!Thread.interrupted()){
+               car.waitForWaxing();
+               System.out.println("Wax Off!");
+               TimeUnit.MILLISECONDS.sleep(200);
+               car.buffed();
+           }
+       }catch(InterruptedException e){
+           System.out.println(e);
+       }
+       System.out.println("Ending Wax Off task");
+    }
+}
+
+
 public class WaxOMatic2 {
+    public static void main(String[] args)throws Exception{
+        Car car = new Car();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new WaxOff(car));
+        exec.execute(new WaxOn(car));
+        TimeUnit.SECONDS.sleep(5);
+        exec.shutdownNow();
+    }
 }
