b31f0d5eb7f63e9da14453eaeeb5022342ac90ac
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-11-06 11:15:10 +0800

    16.6.3从Generator中创建数组

--- src/com/thinkingInJava/chapter16/arrays/CountingGenerator.java
+++ src/com/thinkingInJava/chapter16/arrays/CountingGenerator.java
@@ -42,6 +42,10 @@ public class CountingGenerator {
         private short value=0;
         public java.lang.Short next(){return value++;}
     }
+    public static class Integer implements Generator<java.lang.Integer>{
+        private int value=0;
+        public java.lang.Integer next(){return value++;}
+    }
     public static class Long implements Generator<java.lang.Long>{
         private long value=0;
         public java.lang.Long next(){
