2adcb1ba526ebd754f2cb2740c87e6e74a5c90dc
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-11-15 11:55:47 +0800

    12.7java标准异常

---
+++ src/com/thinkingInJava/chapter12/exceptions/NeverCaught.java
@@ -0,0 +1,13 @@
+package com.thinkingInJava.chapter12.exceptions;
+//Ignoring RuntimeExceptions.
+public class NeverCaught {
+    static void f(){
+        throw new RuntimeException("From f()");
+    }
+    static void g(){
+        f();
+    }
+    public static void main(String[] args){
+        g();
+    }
+}
