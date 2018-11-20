549cb4b23d4689bf07ec1a0952f0f23485bf7b28
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-11-20 11:51:33 +0800

    12.10构造器 12.11异常匹配 12.12其他可选方式 12.13异常使用指南 12.14总结

---
+++ src/com/thinkingInJava/chapter12/exceptions/Human.java
@@ -0,0 +1,34 @@
+package com.thinkingInJava.chapter12.exceptions;
+// Catching exception hierarchies.
+
+class Annoyance extends Exception{ }
+class Sneeze extends Annoyance{ }
+
+public class Human {
+    public static void main(String[] args){
+        //Catch the exact type:
+        try{
+            throw new Sneeze();
+        }catch(Sneeze s){
+            System.out.println("Caught Sneeze");
+        }catch(Annoyance a){
+            System.out.println("Caught  Annoyance");
+        }
+
+        //Catch the base type:
+        try{
+            throw new Sneeze();
+        }catch(Annoyance a){
+            System.out.println("Caught Annoyance");
+        }
+
+//        //Catch the exact type:
+//        try{
+//            throw new Sneeze();
+//        }catch(Annoyance a){
+//            System.out.println("Caught  Annoyance");
+//        }catch(Sneeze s){
+//            System.out.println("Caught Sneeze");
+//        }
+    }
+}
