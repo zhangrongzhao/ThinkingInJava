904ab89377adfd379d5d79298590a1a377b7583f
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-11-26 10:57:47 +0800

    8.4协变返回类型

---
+++ src/com/thinkingInJava/chapter08/polymorphism/CovariantReturn.java
@@ -0,0 +1,29 @@
+package com.thinkingInJava.chapter08.polymorphism;
+
+class Grain{
+    public String toString(){return "Grain";}
+}
+
+class Wheat extends Grain{
+    public String toString(){return "Wheat";}
+}
+
+class Mill{
+    Grain process(){return new Grain();}
+}
+
+class WheatMill extends Mill{
+    Wheat process(){ return new Wheat();}
+}
+
+public class CovariantReturn {
+    public static void main(String[] args){
+        Mill m=new Mill();
+        Grain g=m.process();
+        System.out.println(g);
+        m=new WheatMill();
+        g=m.process();
+        System.out.println(g);
+    }
+
+}
