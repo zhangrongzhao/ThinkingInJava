8a1bdcacb2246caa1d0ba14dfc42b2f976426256
zhangrongzhao <rongzhao.zhang@allcure.cn>
2018-12-11 11:38:51 +0800

    5.3缺省构造器

---
+++ src/com/thinkingInJava/chapter05/initialization/DefaultConstructor.java
@@ -0,0 +1,11 @@
+package com.thinkingInJava.chapter05.initialization;
+
+class Bird{}
+
+
+
+public class DefaultConstructor {
+    public static void main(String[] args){
+        Bird b=new Bird();
+    }
+}
